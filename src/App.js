6a4b2f79b3c76b469656078975d9440e0b15110f
Shubham <shubhamdwip@gmail.com>
2024-09-03 14:11:05 +0530

    commiting untracked files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route }  from 'react-router-dom';
+import Shop from '../src/pages/Shop';
+import ShopCategory from '../src/pages/ShopCategory';
+import Product from '../src/pages/Product';
+import Cart from '../src/pages/Cart'; 
+import LoginSignup from '../src/pages/LoginSignup';
+import Footer from './components/Footer/Footer';
+import men_banner from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kid_banner from './components/Assets/banner_kids.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid"/>} />
+          <Route path="product" element={<Product/>}/>
+            {/* <Route path=':productId' element={<Product/>}/> */}
+            <Route path="/product/:productId" element={<Product />} />
+          <Route path="/Cart" element={<Cart/>}/>
+          <Route path="/login" element={<LoginSignup/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
